a5bf181acc2a898f2d3e331717a9b3fa6eca225e
Jose Maria Flores Zazo <jmfloreszazo@gmail.com>
2020-09-16 07:49:58 +0200

    Add connection with last driver for .NetStandar Class & Core consumer

---
+++ Neo2JDemo2Library/HelloWorldExample.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using Neo4j.Driver;
+
+namespace Neo2JDemo2Library
+{
+    public class HelloWorldExample : IDisposable
+    {
+        private readonly IDriver _driver;
+
+        public HelloWorldExample(string uri, string user, string password)
+        {
+            _driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
+        }
+
+        public void PrintGreeting(string message)
+        {
+            using (var session = _driver.Session())
+            {
+                var greeting = session.WriteTransaction(tx =>
+                {
+                    var result = tx.Run("CREATE (a:Greeting) " +
+                                        "SET a.message = $message " +
+                                        "RETURN a.message + ', from node ' + id(a)",
+                        new { message });
+                    return result.Single()[0].As<string>();
+                });
+                Console.WriteLine(greeting);
+            }
+        }
+
+        public void Dispose()
+        {
+            _driver?.Dispose();
+        }
+
+        public static void Main()
+        {
+
+        }
+    }
+}
---
+++ Neo4JDemo2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Neo2JDemo2Library;
+
+namespace Neo4JDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var greeter = new HelloWorldExample("bolt://localhost:7687", "neo4j", "12345"))
+            {
+                greeter.PrintGreeting("hello, world");
+            }
+        }
+    }
+}
